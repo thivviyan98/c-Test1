72ff3cd29c870150e416f0932d475c2021838860
thivviyan98 <thivviyan1998@gmail.com>
2024-05-07 01:16:15 +0530

    Add project files.

---
+++ ConsoleApp2_test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_test
+{
+    internal class Program
+    {
+        private static object console;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
